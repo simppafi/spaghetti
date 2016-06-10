3b00a32f874df83d52924673502469d9b2fa86c4
Simo Santavirta <simo@simppa.fi>
2016-06-10 23:04:19 +0300

    spaghetti second edition

---
+++ spaghetti2.js
@@ -0,0 +1,12 @@
+
+// THIS METHOD SHOULD RETURN ITS PARAMETER.
+var spaghetti2 = function(inp)
+{
+	if(inp === "al dente")
+	{
+		return inp
+	}
+	return inp;
+}
+
+console.log(spaghetti2("spaghetti2")); 
